221d232b1fe8238528a3cab259dcd7332d629109
张文洁 <50493215@qq.com>
2019-12-03 12:35:41 +0800

    (*)更新编辑页面传入图片 伸缩放

--- KKCamera/EditViewController.m
+++ KKCamera/EditViewController.m
@@ -10,7 +10,7 @@
 #import "SettingViewController.h"
 #import <Masonry.h>
 
-@interface EditViewController ()
+@interface EditViewController () <UIScrollViewDelegate>
 
 @end
 
@@ -22,6 +22,9 @@
     UIButton *_nextBtn;
     UIButton *_resetBtn;
     UIView *_editorView;
+    UIView *_groupView;
+    UIScrollView *_imageScrollView;
+    UIImageView *_imageView;
 }
 
 - (void)viewDidLoad {
@@ -64,6 +67,32 @@
         make.size.mas_equalTo(itemHeight + gap * 2);
     }];
 
+    _groupView = [[UIView alloc] init];
+    [self.contentView addSubview:_groupView];
+    [_groupView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.contentView);
+        make.bottom.equalTo(_editorView.mas_top);
+        make.size.mas_equalTo(100);
+    }];
+    
+    _imageScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, _settingBtn.bounds.size.height, self.contentView.frame.size.width, self.contentView.frame.size.height - (itemHeight + gap * 2 + 100) - _settingBtn.bounds.size.height)];
+    [self.contentView addSubview:_imageScrollView];
+    if (@available(iOS 11.0, *)) {
+        _imageScrollView.contentInsetAdjustmentBehavior =  UIScrollViewContentInsetAdjustmentNever;
+    }
+    _imageScrollView.delegate = self;
+    _imageScrollView.minimumZoomScale = 1.0f;
+    _imageScrollView.maximumZoomScale = 3.0f;
+    _imageScrollView.showsVerticalScrollIndicator = NO;
+    _imageScrollView.showsHorizontalScrollIndicator = NO;
+    
+    _imageView = [[UIImageView alloc] initWithFrame:_imageScrollView.bounds];
+    [_imageScrollView addSubview:_imageView];
+    _imageView.contentMode = UIViewContentModeScaleAspectFit;
+    _imageView.userInteractionEnabled = YES;
+    _imageView.clipsToBounds = YES;
+    [_imageView setImage:_oriImage];
+    
     int position = 0;
     int tag = 1;
     NSArray *items = @[@"cut",@"film",@"filter",@"light",@"edit",@"texture"];
@@ -73,12 +102,58 @@
         [button setImage:[UIImage imageNamed:[NSString stringWithFormat:@"kk_%@",name]] forState:UIControlStateNormal];
         [button setImage:[UIImage imageNamed:[NSString stringWithFormat:@"kk_%@_selected",name]] forState:UIControlStateSelected];
         button.tag = tag;
+        [button addTarget:self action:@selector(onSelectEditorItem:) forControlEvents:UIControlEventTouchUpInside];
         [_editorView addSubview:button];
         tag++;
         position += itemHeight;
     }
-    UIButton *button = [_editorView viewWithTag:1];
-    [button setSelected:YES];
+    [self selectEditorItemWithIndex:0];
+}
+
+#pragma mark -- UIScrollViewDelegate
+
+//返回需要缩放的视图控件 缩放过程中
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return _imageView;
+}
+
+//开始缩放
+- (void)scrollViewWillBeginZooming:(UIScrollView *)scrollView withView:(UIView *)view{
+    NSLog(@"开始缩放");
+}
+//结束缩放
+- (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(CGFloat)scale{
+    NSLog(@"结束缩放");
+}
+
+//缩放中
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    // 延中心点缩放
+    CGFloat imageScaleWidth = scrollView.zoomScale * _imageScrollView.bounds.size.width;
+    CGFloat imageScaleHeight = scrollView.zoomScale * _imageScrollView.bounds.size.height;
+    CGFloat imageX = 0;
+    CGFloat imageY = 0;
+    if (imageScaleWidth < _imageScrollView.bounds.size.width) {
+        imageX = floorf((_imageScrollView.bounds.size.width - imageScaleWidth) / 2.0);
+    }
+    if (imageScaleHeight < _imageScrollView.bounds.size.height) {
+        imageY = floorf((_imageScrollView.bounds.size.height - imageScaleHeight) / 2.0);
+    }
+    _imageView.frame = CGRectMake(imageX, imageY, imageScaleWidth, imageScaleHeight);
+}
+
+- (IBAction)onSelectEditorItem:(UIButton *)sender{
+    [self selectEditorItemWithIndex:(int)sender.tag - 1];
+}
+
+- (void)selectEditorItemWithIndex:(int)index{
+    for (UIButton *button in _editorView.subviews) {
+        if (index + 1 == button.tag) {
+            [button setSelected:YES];
+        }else{
+            [button setSelected:NO];
+        }
+    }
 }
 
 -(void)viewSafeAreaInsetsDidChange{
@@ -88,11 +163,17 @@
     [bottomView setBackgroundColor:[UIColor colorWithRed:0.114 green:0.133 blue:0.137 alpha:1.000]];
     [self.view insertSubview:bottomView atIndex:0];
     
-    [_editorView updateConstraintsIfNeeded];
+    CGRect scrollTemp = _imageScrollView.frame;
+    scrollTemp.size.height = _imageScrollView.frame.size.height - safeAreaInsets.top - safeAreaInsets.bottom;
+    _imageScrollView.frame = scrollTemp;
+    [_imageView setFrame:_imageScrollView.bounds];
 }
 
 -(void)setOriginImage:(UIImage *)originImage{
     _oriImage = originImage;
+    if (_imageView) {
+        _imageView.image = _oriImage;
+    }
 }
 
 -(IBAction)onSetting:(id)sender{
