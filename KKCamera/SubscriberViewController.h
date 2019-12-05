72a69126c168073f4a2471c5bc9ffbbe5db03079
张文洁 <50493215@qq.com>
2019-12-05 13:42:52 +0800

    (*)更新编辑页面传入图片

---
+++ KKCamera/SubscriberViewController.h
@@ -0,0 +1,17 @@
+//
+//  SubscriberViewController.h
+//  KKCamera
+//
+//  Created by Jam Zhang on 2019/12/4.
+//  Copyright © 2019 Jam Zhang. All rights reserved.
+//
+
+#import "BasicViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SubscriberViewController : BasicViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KKCamera/SubscriberViewController.m
@@ -0,0 +1,32 @@
+//
+//  SubscriberViewController.m
+//  KKCamera
+//
+//  Created by Jam Zhang on 2019/12/4.
+//  Copyright © 2019 Jam Zhang. All rights reserved.
+//
+
+#import "SubscriberViewController.h"
+
+@interface SubscriberViewController ()
+
+@end
+
+@implementation SubscriberViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
